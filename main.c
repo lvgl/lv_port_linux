b475d05a4f26bbf24cc61e0bade047c3e1ec0804
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2019-07-05 05:42:32 +0200

    add comments

--- main.c
+++ main.c
@@ -5,10 +5,6 @@
 
 #define DISP_BUF_SIZE (80*LV_HOR_RES_MAX)
 
-void dummy_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_p)
-{
-    lv_disp_flush_ready(drv);
-}
 int main(void)
 {
     /*LittlevGL init*/
@@ -17,16 +13,19 @@ int main(void)
     /*Linux frame buffer device init*/
     fbdev_init();
 
+    /*A small buffer for LittlevGL to draw the screen's content*/
     static lv_color_t buf[DISP_BUF_SIZE];
+
+    /*Initialize a descriptor for the buffer*/
     static lv_disp_buf_t disp_buf;
     lv_disp_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
-//
+
+    /*Initialize and register a display driver*/
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.buffer = &disp_buf;
     disp_drv.flush_cb = fbdev_flush;
     lv_disp_drv_register(&disp_drv);
-
 
     /*Create a Demo*/
     demo_create();
@@ -34,10 +33,11 @@ int main(void)
 
     /*Handle LitlevGL tasks (tickless mode)*/
     while(1) {
-        lv_tick_inc(5);
+        lv_tick_inc(5);         /*You can add it to a pthread the have better timing */
         lv_task_handler();
         usleep(5000);
     }
 
     return 0;
 }
+
