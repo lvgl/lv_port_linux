23d0db627894ec15743bb5eb7126d55404c5aa86
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2022-03-25 13:59:48 +0100

    update to lvgl v8.2

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
 #include "lv_drivers/display/fbdev.h"
-#include "lv_demos/lv_demo.h"
+#include "lv_drivers/indev/evdev.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <time.h>
@@ -32,12 +33,29 @@ int main(void)
     disp_drv.ver_res    = 480;
     lv_disp_drv_register(&disp_drv);
 
+    evdev_init();
+    static lv_indev_drv_t indev_drv_1;
+    lv_indev_drv_init(&indev_drv_1); /*Basic initialization*/
+    indev_drv_1.type = LV_INDEV_TYPE_POINTER;
+
+    /*This function will be called periodically (by the library) to get the mouse position and state*/
+    indev_drv_1.read_cb = evdev_read;
+    lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
+
+
+    /*Set a cursor for the mouse*/
+    LV_IMG_DECLARE(mouse_cursor_icon)
+    lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
+    lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+    lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
+
+
     /*Create a Demo*/
     lv_demo_widgets();
 
     /*Handle LitlevGL tasks (tickless mode)*/
     while(1) {
-        lv_task_handler();
+        lv_timer_handler();
         usleep(5000);
     }
 
