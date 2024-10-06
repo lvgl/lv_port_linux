dd7f135ce358789a1dac3c6276e4fad2fa7e678a
Richard Schleich <rs@noreya.tech>
2024-10-06 21:44:18 +0200

    Adds support for evdev input pointer device to the example Updates README

--- main.c
+++ main.c
@@ -11,12 +11,34 @@ static const char *getenv_default(const char *name, const char *dflt)
     return getenv(name) ? : dflt;
 }
 
+#if LV_USE_EVDEV
+static void lv_linux_init_input_pointer(lv_display_t *disp)
+{
+    // Enables a pointer (touchscreen/mouse) input device
+    // Use 'evtest' to find the correct input device. /dev/input/by-id/ is recommeded if possible
+    // Use /dev/input/by-id/my-mouse-or-touchscreen or /dev/input/eventX
+    const char *input_device = getenv_default("LV_LINUX_EVDEV_POINTER_DEVICE", "/dev/input/by-id/my-mouse-or-touchscreen");
+    lv_indev_t *touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, input_device); 
+    lv_indev_set_display(touch, disp);
+
+    // Disable this if you want no cursor
+    LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image source.*/
+    lv_obj_t * cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
+    lv_image_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
+    lv_indev_set_cursor(touch, cursor_obj); /*Connect the image  object to the driver*/
+}
+#endif
+
 #if LV_USE_LINUX_FBDEV
 static void lv_linux_disp_init(void)
 {
     const char *device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
     lv_display_t * disp = lv_linux_fbdev_create();
-
+    
+    #if LV_USE_EVDEV
+    lv_linux_init_input_pointer(disp);
+    #endif
+    
     lv_linux_fbdev_set_file(disp, device);
 }
 #elif LV_USE_LINUX_DRM
@@ -24,6 +46,10 @@ static void lv_linux_disp_init(void)
 {
     const char *device = getenv_default("LV_LINUX_DRM_CARD", "/dev/dri/card0");
     lv_display_t * disp = lv_linux_drm_create();
+
+    #if LV_USE_EVDEV
+    lv_linux_init_input_pointer(disp);
+    #endif
 
     lv_linux_drm_set_file(disp, device, -1);
 }
