47fb5bd51c79cd626ca608b452e34f5aaffe855e
Prabhu Sundararaj <prabhu.sundararaj@nxp.com>
2025-03-11 17:11:28 -0500

    Add support for direct rendering based on linux dma-buf
    Signed-off-by: Prabhu Sundararaj <prabhu.sundararaj@nxp.com>
    Signed-off-by: Cosmin Radu <cosmin.radu_1@nxp.com>

--- scripts/gen_wl_protocols.sh
+++ scripts/gen_wl_protocols.sh
@@ -2,7 +2,7 @@
 # Generate wayland xdg shell protocol
 
 OUTPUT_DIR="$1"
-PROTOCOL_ROOT="${SYSROOT:-}/usr/share/wayland-protocols"
+PROTOCOL_ROOT="${SDKTARGETSYSROOT:-}/usr/share/wayland-protocols"
 
 if ! test -d $PROTOCOL_ROOT
 then
@@ -16,6 +16,9 @@ then
 	mkdir $OUTPUT_DIR
 	wayland-scanner client-header "$PROTOCOL_ROOT/stable/xdg-shell/xdg-shell.xml" "$OUTPUT_DIR/wayland_xdg_shell.h"
 	wayland-scanner private-code  "$PROTOCOL_ROOT/stable/xdg-shell/xdg-shell.xml" "$OUTPUT_DIR/wayland_xdg_shell.c"
+	wayland-scanner client-header "$PROTOCOL_ROOT/stable/linux-dmabuf/linux-dmabuf-v1.xml" "$OUTPUT_DIR/wayland_linux_dmabuf.h"
+	wayland-scanner private-code "$PROTOCOL_ROOT/stable/linux-dmabuf/linux-dmabuf-v1.xml" "$OUTPUT_DIR/wayland_linux_dmabuf.c"
 fi
 
 echo "$OUTPUT_DIR/wayland_xdg_shell.c"
+echo "$OUTPUT_DIR/wayland_linux_dmabuf.c"
