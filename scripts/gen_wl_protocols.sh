731c12a4f6d0812d9a0d250f6fe61cbd27a2c81d
Nicușor Cîțu <nicusor.citu@nxp.com>
2025-03-18 15:16:41 +0200

    fix(scripts) Fixed the wayland-protocols path.
    Signed-off-by: Nicușor Cîțu <nicusor.citu@nxp.com>

--- scripts/gen_wl_protocols.sh
+++ scripts/gen_wl_protocols.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Generate wayland xdg shell protocol
 
-if ! test -d /usr/share/wayland-protocols
+if ! test -d "$SDKTARGETSYSROOT/usr/share/wayland-protocols"
 then
 	exit 1
 fi
@@ -9,8 +9,8 @@ fi
 if ! test -d wl_protocols
 then
 	mkdir wl_protocols
-	wayland-scanner client-header "/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml" "wl_protocols/wayland_xdg_shell.h"
-	wayland-scanner private-code "/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml" "wl_protocols/wayland_xdg_shell.c"
+	wayland-scanner client-header "$SDKTARGETSYSROOT/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml" "wl_protocols/wayland_xdg_shell.h"
+	wayland-scanner private-code "$SDKTARGETSYSROOT/usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml" "wl_protocols/wayland_xdg_shell.c"
 fi
 
 printf "wl_protocols/wayland_xdg_shell.c"
