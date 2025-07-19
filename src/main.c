eb7ec181e7d74689be8fa20127af095c4f76ed6a
Alex <aleksandrdorofeev04@gmail.com>
2025-07-19 15:19:38 +0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -108,8 +108,10 @@ static void configure_simulator(int argc, char **argv)
 
 static lv_timer_t* minute_timer;
 
-static void minute_tick(lv_timer_t* timer __attribute__((unused)))
+static void minute_tick(lv_timer_t* timer)
 {
+    (void)timer;
+    
     time_t now = time(NULL);
     struct tm* t = localtime(&now);
     if (t->tm_sec == 0) // Trigger only when seconds are 0
