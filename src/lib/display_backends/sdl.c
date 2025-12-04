c9487af78d1b1f8bfad694880039ad3bb991f5fc
André <andre_miguel_costa@hotmail.com>
2025-12-04 11:20:04 +0100

    feat(sdl): add indev support to sdl backend

--- src/lib/display_backends/sdl.c
+++ src/lib/display_backends/sdl.c
@@ -89,13 +89,30 @@ int backend_init_sdl(backend_t *backend)
  */
 static lv_display_t *init_sdl(void)
 {
-    lv_display_t *disp;
 
-    disp = lv_sdl_window_create(settings.window_width, settings.window_height);
+    lv_display_t * disp = lv_sdl_window_create(settings.window_width, settings.window_height);
 
     if (disp == NULL) {
         return NULL;
     }
+
+    lv_indev_t * mouse = lv_sdl_mouse_create();
+    lv_indev_set_group(mouse, lv_group_get_default());
+    lv_indev_set_display(mouse, disp);
+
+    LV_IMAGE_DECLARE(mouse_cursor_icon);
+    lv_obj_t * cursor_obj;
+    cursor_obj = lv_image_create(lv_screen_active());
+    lv_image_set_src(cursor_obj, &mouse_cursor_icon);
+    lv_indev_set_cursor(mouse, cursor_obj);
+
+    lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
+    lv_indev_set_display(mousewheel, disp);
+    lv_indev_set_group(mousewheel, lv_group_get_default());
+
+    lv_indev_t * kb = lv_sdl_keyboard_create();
+    lv_indev_set_display(kb, disp);
+    lv_indev_set_group(kb, lv_group_get_default());
 
     return disp;
 }
